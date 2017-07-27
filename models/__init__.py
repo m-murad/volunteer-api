86d874e804f13c5712fba05536efa8b0e50aef6b
m-murad <murad.kuka@gmail.com>
2017-07-28 03:29:26 +0530

    Added ConfirmedVolunteer model.

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from .task import Task
 from .volunteer import UnconfirmedVolunteer
+from .volunteer import ConfirmedVolunteer
--- models/volunteer.py
+++ models/volunteer.py
@@ -4,10 +4,24 @@ from endpoints_proto_datastore.ndb.model import EndpointsModel
 
 
 class UnconfirmedVolunteer(EndpointsModel):
-
     _message_fields_schema = ("id", "applied_on", "name", "email", "sex", "city", "country", "phone_no")
 
     applied_on = ndb.DateTimeProperty(auto_now_add=True)
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+    sex = ndb.StringProperty()
+    city = ndb.StringProperty()
+    country = ndb.StringProperty()
+    phone_no = ndb.StringProperty()
+
+
+class ConfirmedVolunteer(EndpointsModel):
+    _message_fields_schema = ("id", "applied_on", "confirmed_on", "confirmed_by", "name", "email", "sex", "city",
+                              "country", "phone_no")
+
+    applied_on = ndb.DateTimeProperty()
+    confirmed_on = ndb.DateTimeProperty(auto_now_add=True)
+    confirmed_by = ndb.StringProperty()
     name = ndb.StringProperty()
     email = ndb.StringProperty()
     sex = ndb.StringProperty()
